d97e47f4716199318b9820fc9456dbc2210672d2
drew <43808240+drew458@users.noreply.github.com>
2022-05-14 11:09:03 +0200

    create spring starter project

---
+++ LiveFinder/src/main/java/it/uniroma3/siw/livefinder/LiveFinderApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.livefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveFinderApplication.class, args);
+	}
+
+}
---
+++ LiveFinder/src/test/java/it/uniroma3/siw/livefinder/LiveFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.livefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
