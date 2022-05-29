16250e08356c3187efa2dc5bee8cf1914711cf98
drew <43808240+drew458@users.noreply.github.com>
2022-05-29 19:56:13 +0200

    cambiati import e rimossi pojo

--- LiveFinder/src/main/java/it/uniroma3/siw/livefinder/repository/TourRepository.java
+++ LiveFinder/src/main/java/it/uniroma3/siw/livefinder/repository/TourRepository.java
@@ -2,7 +2,7 @@ package it.uniroma3.siw.livefinder.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import it.uniroma3.siw.livefinder.model.pojo.Tour;
+import it.uniroma3.siw.livefinder.model.Tour;
 
 public interface TourRepository extends CrudRepository<Tour, Long>{
     
