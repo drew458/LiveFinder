796d8f15b443f8984b3f72ce27c59ae8f88e2f1b
Antonio Anghelone <Nixsr@users.noreply.github.com>
2022-06-10 14:33:06 +0200

    Aggiunta ricerca dei concerti per citta
    Aggiunta ricerca dei concerti per citta

--- LiveFinder/src/main/java/it/uniroma3/siw/livefinder/service/CittaService.java
+++ LiveFinder/src/main/java/it/uniroma3/siw/livefinder/service/CittaService.java
@@ -1,8 +1,12 @@
 package it.uniroma3.siw.livefinder.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.livefinder.repository.CittaRepository;
 
 @Service
 public class CittaService {
 
+	
 }
