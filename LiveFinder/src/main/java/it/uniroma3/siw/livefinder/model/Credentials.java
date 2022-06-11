ecf579eab80e9d3ead5975e049a2eef790b7ee69
drew <43808240+drew458@users.noreply.github.com>
2022-06-11 17:16:21 +0200

    bugfix

--- LiveFinder/src/main/java/it/uniroma3/siw/livefinder/model/Credentials.java
+++ LiveFinder/src/main/java/it/uniroma3/siw/livefinder/model/Credentials.java
@@ -7,8 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-
-import org.springframework.data.annotation.Transient;
+import javax.persistence.Transient;
 
 @Entity
 public class Credentials {
